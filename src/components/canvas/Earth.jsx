d36e4f3c2d7c502805b403ecc09bd79edef0e6d0
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-07-05 20:59:44 +0400

    ..

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -4,7 +4,7 @@ import {  OrbitControls, Preload,  useGLTF } from '@react-three/drei';
 
 import CanvasLoader from '../Loader'
 const Earth = () => {
-  const earth = useGLTF('../../../dist/planet/scene.gltf')
+  const earth = useGLTF('../../../public/planet/scene.gltf')
   return (
     <primitive
     object={earth.scene}
